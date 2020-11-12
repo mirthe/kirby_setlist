b1e821a90470bdd28d1fc165b2ec937ee28057d8
Mirthe Valentijn <mirthe@aanzee.nl>
2020-11-12 13:32:52 +0100

    Initial Plugin Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Kirby Plugin: Setlist.fm
+
+This plugin allows you to include concert setlists from the Setlist.fm API on your pages.
+
+## Example 
+
+Add the following to your config:
+
+    'setlistfm.apiKey' => 'XX',
+       
+Placed for example with 
+
+    (setlist: show: 398b9b7)
+
+(image: example.png)
+
+## Todo
+
+- Offer as an official Kirby plugin
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+// TODO opschonen en aanbieden?
+// https://getkirby.com/docs/guide/plugins/best-practices
+
+// TODO Aparte git repo of submodule van maken?
+
+// zie https://api.setlist.fm/docs/1.0/resource__1.0_setlist__setlistId_.html
+
+Kirby::plugin('mirthe/setlist', [
+    'options' => [
+        'cache' => true
+    ],
+    'tags' => [
+        'setlist' => [
+            'attr' =>[
+                'show'
+            ],
+            'html' => function($tag) {
+
+                $showid = $tag->show;
+                $api_key = option('setlistfm.apiKey');
+               
+                $url = "https://api.setlist.fm/rest/1.0/setlist/" . $showid;
+                $headers = array(
+                    'x-api-key: '.$api_key, 
+                    'Accept: application/json');
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                $rawdata = curl_exec($ch);
+                curl_close($ch);
+                
+                // print_r($rawdata);
+                // exit();
+
+                // TODO nog iets met lastFmEventId?
+                
+                $setlistjson = json_decode($rawdata,true);
+                
+                $mijnoutput = '<div class="setlist">' ."\n";
+                $mijnoutput .= '<p><a href="'.$setlistjson['url'].'" title="Bekijken op Setlist.fm">'.$setlistjson['artist']['name']."</a>";
+                $mijnoutput .= " in ".$setlistjson['venue']['name']." (".$setlistjson['venue']['city']['name'].")";
+                $mijnoutput .= " op ". $setlistjson['eventDate'].":</p>\n";
+                
+                foreach ($setlistjson['sets'] as $sets) {
+                    if( count($sets) > 0){
+                        $mijnoutput .= "<ul>";
+                        for($i = 0; $i < count($sets); $i++) {
+                            $liedjes = $sets[$i]['song'];
+                            for($j = 0; $j < count($liedjes); $j++) {
+                                if ($liedjes[$j]['name'] !== ''){
+                                    $mijnoutput .= '<li>'. ($liedjes[$j]['name']) . "</li>";
+                                }
+                            }
+                        }
+                        $mijnoutput .= "</ul>\n";
+                    }
+                    else {
+                        $mijnoutput .= "<p><em>De setlist is (nog) niet ingevoerd.</em></p>\n";
+                    }
+                }
+                $mijnoutput .= "</div>\n";
+
+                return $mijnoutput;
+            }
+        ]
+    ]
+]);
+
+?>
