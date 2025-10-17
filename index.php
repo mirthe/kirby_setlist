ac11cbe0c3318a7f562302327c6078e000871998
Mirthe Valentijn <me@mirthe.org>
2025-10-17 18:18:08 +0200

    Tweak

--- index.php
+++ index.php
@@ -18,8 +18,6 @@ Kirby::plugin('mirthe/setlist', [
             ],
             'html' => function($tag) {
                 
-                $site = kirby()->site();
-
                 $showid = $tag->show;
                 $api_key = option('setlistfm.apiKey');
                
@@ -30,7 +28,7 @@ Kirby::plugin('mirthe/setlist', [
                 $ch = curl_init($url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                curl_setopt($ch, CURLOPT_USERAGENT, $site->title());
+                curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
                 $rawdata = curl_exec($ch);
                 curl_close($ch);
                 
